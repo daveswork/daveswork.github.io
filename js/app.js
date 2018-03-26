3984b9c09d384d697b322b8a82f11197ffb4eac2
Daves-Mac <daveswork@users.noreply.github.com>
2018-03-26 12:10:24 -0400

    Added some additional formatting to projects.

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 var main = function() {
   $('.glyphicon').click(function(){
     event.preventDefault();
-	  $(this).toggleClass('glyphicon-plus').toggleClass('glyphicon-minus');
+	  $(this).toggleClass('glyphicon-plus').toggleClass('glyphicon-minus').toggleClass('active');
     $(this).next('.content').toggleClass('hide');
   });
 
